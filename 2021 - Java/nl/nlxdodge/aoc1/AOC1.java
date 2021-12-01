81c31a9aaf4fa01d64858055c1df4773033c4b9e
NLxDoDge <nlxdodge@gmail.com>
2021-12-01 06:51:56 +0100

    2021 - Day 1

--- 2021 - Java/nl/nlxdodge/aoc1/AOC1.java
+++ 2021 - Java/nl/nlxdodge/aoc1/AOC1.java
@@ -3,14 +3,65 @@ package nl.nlxdodge.aoc1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 class AOC1 {
     private static final String FILE_PATH = "./nl/nlxdodge/aoc1/input.txt";
 
     public static void main(String[] args) throws IOException {
+
+        List<Depth> filterDepths = new ArrayList<>();
+
         try (Stream<String> stream = Files.lines(Paths.get(FILE_PATH))) {
-            stream.forEach(String::new);
+            List<Depth> depths = stream.map(Depth::new).toList();
+            List<Depth> sumDepths = new ArrayList<>();
+
+            depths.forEach(depth -> {
+                if (!filterDepths.isEmpty() && filterDepths.get(filterDepths.size() - 1).checkIncrease(depth)) {
+                    depth.up = true;
+                }
+                if (filterDepths.size() >= 2) {
+                    Depth second = filterDepths.get(filterDepths.size() - 1);
+                    Depth third = filterDepths.get(filterDepths.size() - 2);
+                    Depth sumDepth = new Depth(depth.getSum(second, third).toString());
+                    if (!sumDepths.isEmpty() && sumDepths.get(sumDepths.size() - 1).checkIncrease(sumDepth)) {
+                        sumDepth.up = true;
+                    }
+                    sumDepths.add(sumDepth);
+                }
+
+                filterDepths.add(depth);
+            });
+
+            long result1 = filterDepths.stream().filter(d -> d.up).count();
+            long result2 = sumDepths.stream().filter(d -> d.up).count();
+
+            Logger.getGlobal().info(() -> String.format("Result 1: %s", result1));
+            Logger.getGlobal().info(() -> String.format("Result 2: %s", result2));
         }
+    }
+}
+
+class Depth {
+    int x;
+    boolean up;
+
+    public Depth(String x) {
+        this.x = Integer.parseInt(x);
+    }
+
+    public boolean checkIncrease(Depth depth) {
+        return x < depth.x;
+    }
+
+    public Integer getSum(Depth d1, Depth d2) {
+        return x + d1.x + d2.x;
+    }
+
+    public String toString() {
+        return "Depth: " + x + " Up: " + up + "\r\n";
     }
 }
