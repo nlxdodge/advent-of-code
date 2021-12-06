e161bfaa4a126b61e2c1d081eccfbe18ef1a3cb0
NLxDoDge <nlxdodge@gmail.com>
2021-12-06 16:42:37 +0100

    2021 - Day 6

--- 2021 - Java/README.md
+++ 2021 - Java/README.md
@@ -1,5 +1,22 @@
 # Advent of code year 2021
 
+Works with Java 17 or higher.
+
+For example I use:
+```java
+stream.toList();
+```
+Which doesn't exist yet in Java 11. If you do however want to run it in Java 11 change the following line of:
+
+```java
+List<String> list = stream.toList();
+```
+To:
+
+```java
+List<String> list = stream.map(String::new).collect(Collectors.toList());
+```
+
 Because I started working this year at Sogeti I wanted to make my java skills better.
 While making a quick java file that could solve the puzzel, I would always refactor some of the code to be better afterwards.
 While also getting all the stars this year and waking up early to try out my skills when I am completely 'awake'.
@@ -7,9 +24,9 @@ While also getting all the stars this year and waking up early to try out my ski
 1. ⭐⭐
 1. ⭐⭐
 1. ⭐⭐
-1. 
-1. 
-1. 
+1. ⭐⭐
+1. ⭐⭐
+1. ⭐⭐
 1. 
 1. 
 1. 
--- 2021 - Java/nl/nlxdodge/aoc6/AOC6.java
+++ 2021 - Java/nl/nlxdodge/aoc6/AOC6.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AOC6 {
@@ -17,51 +17,58 @@ public class AOC6 {
         try (Stream<String> stream = Files.lines(Paths.get(FILE_PATH))) {
             List<String> list = stream.toList();
 
-            int days = 256;
-
-            List<Byte> school = new ArrayList<>();
-            for (String line : list) {
-                List<String> spawns = List.of(line.split(","));
-                for (String spawn : spawns) {
-                    school.add(Byte.parseByte(spawn));
-                }
-            }
-
-            List<Byte> currentFish = new ArrayList<>();
-            for (int day = 1; day < days + 1; day++) {
-                for (Byte timer : school) {
-                    if (timer == 0) {
-                        currentFish.add(handleFish(timer));
-                        currentFish.add((byte) 8);
-                    } else {
-                        currentFish.add(handleFish(timer));
-                    }
-                }
-                school.clear();
-                school.addAll(currentFish);
-                currentFish.clear();
-
-                if(school.size() > 26984457539) {
-                    System.out.println("Should have");
-                }
-            }
-
-            final String result1 = String.format("%s", school.size());
+            Map<Integer, Long> school = initializeMap(list);
+            Long result1 = simulateDays(80, school);
             Logger.getGlobal().info(() -> String.format("Result 1: %s", result1));
 
-            // code for part two
-
-            String result2 = "";
+            Map<Integer, Long> school2 = initializeMap(list);
+            Long result2 = simulateDays(256, school2);
             Logger.getGlobal().info(() -> String.format("Result 2: %s", result2));
         }
     }
 
-    public static byte handleFish(byte timer) {
-        if (timer == 0) {
-            return 6;
-        } else if (timer >= 1) {
-            return --timer;
+    public static Long simulateDays(int days, Map<Integer, Long> school) {
+        Map<Integer, Long> newSchool = new HashMap<>();
+        for (int day = 1; day < days + 1; day++) {
+            for (Map.Entry<Integer, Long> fish : school.entrySet()) {
+                int spawn = fish.getKey();
+                if (spawn == 0) {
+                    newSchool.put(8, school.get(0) != null ? school.get(0) : 0);
+                } else {
+                    int newDay = fish.getKey() - 1;
+                    if (school.get(newDay) == null && school.get(spawn) >= 1) {
+                        newSchool.put(newDay, school.get(spawn));
+                    } else {
+                        long oldNumb = school.get(spawn) != null ? school.get(spawn) : 0;
+                        newSchool.put(newDay, oldNumb);
+                    }
+                }
+            }
+            long old0 = school.get(0) != null ? school.get(0) : 0;
+            long old7 = school.get(7) != null ? school.get(7) : 0;
+            newSchool.put(6, old0 + old7);
+
+            school.clear();
+            school.putAll(newSchool);
+            newSchool.clear();
         }
-        return timer;
+        return mapSize(school);
+    }
+
+    public static Map<Integer, Long> initializeMap(List<String> list) {
+        Map<Integer, Long> school = new HashMap<>(
+                Map.of(0, 0L, 1, 0L, 2, 0L, 3, 0L, 4, 0L, 5, 0L, 6, 0L, 7, 0L, 8, 0L));
+        for (String line : list) {
+            List<String> spawns = List.of(line.split(","));
+            for (String spawn : spawns) {
+                int number = Integer.parseInt(spawn);
+                school.put(number, school.get(number) + 1);
+            }
+        }
+        return school;
+    }
+
+    public static long mapSize(Map<Integer, Long> school) {
+        return school.values().stream().mapToLong(Long::longValue).sum();
     }
 }
