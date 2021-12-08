a58c9696bb7a4f5ea15d8c6f5e3cbefd15860bee
NLxDoDge <nlxdodge@gmail.com>
2021-12-08 19:58:31 +0100

    2021 - Day 8

--- 2021 - Java/nl/nlxdodge/aoc6/AOC6.java
+++ 2021 - Java/nl/nlxdodge/aoc6/AOC6.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AOC6 {
