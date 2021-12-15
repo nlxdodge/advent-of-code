e62ed8d1ebc3a32a4a969b37c27aa25b2ffe3eab
NLxDoDge <nlxdodge@gmail.com>
2021-12-15 20:19:00 +0100

    2021 - Day 15

--- 2021 - Java/nl/nlxdodge/aoc15/AOC15.java
+++ 2021 - Java/nl/nlxdodge/aoc15/AOC15.java
@@ -1,11 +1,16 @@
 package nl.nlxdodge.aoc15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
@@ -18,101 +23,102 @@ public class AOC15 {
             List<String> list = stream.toList();
 
             Node[][] map = initializeMap(list);
-            // code for part one
 
-            Node smallestPath = aStar(map);
-            printPath(smallestPath, map);
-            System.out.println("WAA");
+            List<Node> smallestPath = aStar(map);
 
-            // Long path = getLowestRiskPath(map);
-
-            Long result1 = (long) list.size();
+            Long result1 = (long) smallestPath
+                    .subList(0, smallestPath.size() - 1)
+                    .stream()
+                    .mapToLong(n -> n.value)
+                    .sum();
             System.out.println(String.format("Result 1: %s", result1));
 
-            // code for part two
+            Node[][] biggerMap = resizeMap(map, 5);
+            List<Node> smallestPathBiggerMap = aStar(biggerMap);
 
-            String result2 = "";
+            Long result2 = (long) smallestPathBiggerMap
+                    .subList(0, smallestPathBiggerMap.size() - 1)
+                    .stream()
+                    .mapToLong(n -> n.value)
+                    .sum();
             System.out.println(String.format("Result 2: %s", result2));
         }
     }
 
-    public static void printPath(Node target, Node[][] map) {
-        Node n = target;
-        List<Node> nodes = new ArrayList<>();
-        while (n.parent != null) {
-            nodes.add(n);
-            n = n.parent;
-        }
-        nodes.add(n);
-
-        for (Node node : nodes) {
-            map[node.y][node.x].value = 0;
-        }
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                String toPrint = map[y][x].value == 0 ? "█" : "" + map[y][x].value;
-                System.out.print(toPrint);
-            }
-            System.out.println("");
-        }
-        System.out.println("----------");
-    }
-
-    private static Node aStar(Node[][] map) {
+    private static List<Node> aStar(Node[][] map) {
         Node start = map[0][0];
         Node target = map[map[0].length - 1][map.length - 1];
 
-        PriorityQueue<Node> closedNodes = new PriorityQueue<>();
-        PriorityQueue<Node> openNodes = new PriorityQueue<>(List.of(start));
+        Map<Node, Node> cameFrom = new LinkedHashMap<>();
+        PriorityQueue<Node> openSet = new PriorityQueue<>(List.of(start));
 
-        while (!openNodes.isEmpty()) {
-            Node n = openNodes.peek();
-            if (n == target) {
-                return n;
+        while (!openSet.isEmpty()) {
+            Node current = openSet.stream().min(Node::compareTo).get();
+            if (current == target) {
+                return reConstructPath(cameFrom, current);
             }
-            List<Node> neighbours = getNeighbours(map, n);
+            openSet.remove(current);
+            List<Node> neighbours = getNeighbours(map, current);
             for (Node m : neighbours) {
-                Long totalWeight = n.g + m.value;
-                if (!closedNodes.contains(m) && !openNodes.contains(m)) {
-                    m.parent = n;
-                    m.g = totalWeight;
-                    m.f = m.g + m.value;
-                    openNodes.add(m);
-                } else {
-                    printPath(n, map);
-                    if (totalWeight < m.g) {
-                        m.parent = n;
-                        m.g = totalWeight;
-                        m.f = m.g + m.value;
-
-                        if (closedNodes.contains(m)) {
-                            closedNodes.remove(m);
-                            openNodes.add(m);
-                        }
-                        
+                Long gScore = current.g + m.value;
+                if (gScore < m.g) {
+                    cameFrom.put(m, current);
+                    m.g = gScore;
+                    m.f = gScore + m.value;
+                    if (!openSet.contains(m)) {
+                        openSet.add(m);
                     }
                 }
             }
-            openNodes.remove(n);
-            closedNodes.add(n);
         }
-        return null;
+        throw new RuntimeException("Couldn't find target in A-star algorithm");
+    }
+
+    private static List<Node> reConstructPath(Map<Node, Node> cameFrom, Node current) {
+        List<Node> path = new ArrayList<>(List.of(current));
+        while (cameFrom.containsKey(current)) {
+            current = cameFrom.get(current);
+            path.add(current);
+        }
+        return path;
+    }
+
+    public static void printMap(List<Node> path, Node[][] map) {
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map.length; x++) {
+                System.out.print(path.contains(map[y][x]) ? "█" : map[y][x].value);
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void save(List<Node> path, Node[][] map) {
+        try (PrintStream output = new PrintStream(new File("./nl/nlxdodge/aoc15/output.txt"))) {
+            for (int y = 0; y < map.length; y++) {
+                StringBuilder line = new StringBuilder();
+                for (int x = 0; x < map.length; x++) {
+                    line.append(path.contains(map[y][x]) ? "█" : map[y][x].value);
+                }
+                output.println(line.toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private static List<Node> getNeighbours(Node[][] map, Node node) {
         List<Node> neighbours = new ArrayList<>();
-        // 4 of em
-        if (node.y + 1 < map.length) {
-            neighbours.add(map[node.y + 1][node.x]);
-        }
-        if (node.y - 1 > 0) {
-            neighbours.add(map[node.y - 1][node.x]);
-        }
-        if (node.x + 1 < map[0].length) {
-            neighbours.add(map[node.y][node.x + 1]);
+        if (node.x + 1 < map.length) {
+            neighbours.add(map[node.x + 1][node.y]);
         }
         if (node.x - 1 > 0) {
-            neighbours.add(map[node.y][node.x - 1]);
+            neighbours.add(map[node.x - 1][node.y]);
+        }
+        if (node.y + 1 < map[0].length) {
+            neighbours.add(map[node.x][node.y + 1]);
+        }
+        if (node.y - 1 > 0) {
+            neighbours.add(map[node.x][node.y - 1]);
         }
         return neighbours;
     }
@@ -120,12 +126,29 @@ public class AOC15 {
     private static Node[][] initializeMap(List<String> list) {
         Node[][] map = new Node[list.size()][list.get(0).length()];
         for (int y = 0; y < list.size(); y++) {
-            String line = list.get(y);
             for (int x = 0; x < list.get(0).length(); x++) {
+                String line = list.get(y);
                 map[y][x] = new Node(y, x, Integer.parseInt("" + line.charAt(x)));
             }
         }
         return map;
+    }
+
+    private static Node[][] resizeMap(Node[][] map, int times) {
+        Node[][] biggerMap = new Node[map.length * times][map.length * times];
+        for (int yT = 0; yT < times; yT++) {
+            for (int xT = 0; xT < times; xT++) {
+                for (int y = 0; y <= map.length - 1; y++) {
+                    for (int x = 0; x <= map.length - 1; x++) {
+                        Integer newValue = map[y][x].value + yT + xT;
+                        newValue = (newValue >= 10 ? (newValue % 10) + 1 : newValue);
+                        Node newNode = new Node(x + (xT * map.length), y + (yT * map.length), newValue);
+                        biggerMap[x + (xT * map.length)][y + (yT * map.length)] = newNode;
+                    }
+                }
+            }
+        }
+        return biggerMap;
     }
 }
 
@@ -133,9 +156,8 @@ class Node implements Comparable<Node> {
     Integer y = 0;
     Integer x = 0;
     Integer value = 0;
-    Node parent;
-    Long g = -20L;
-    Long f = -20L;
+    Long g = Long.MAX_VALUE;
+    Long f = Long.MAX_VALUE;
 
     public Node(Integer x, Integer y, Integer value) {
         this.x = x;
@@ -145,10 +167,10 @@ class Node implements Comparable<Node> {
 
     @Override
     public int compareTo(Node node) {
-        return Long.compare(this.f, f);
+        return Long.compare(f, node.f);
     }
 
     public String toString() {
-        return String.format("(Y%s,X%s = V%s T%s)", y, x, value, g);
+        return String.format("(Y%s,X%s = V%s G%s F%s)", y, x, value, g, f);
     }
 }
