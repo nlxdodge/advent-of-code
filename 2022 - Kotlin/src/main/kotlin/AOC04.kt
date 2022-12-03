2d6a09da3db7821de716c696c242ce382e65cd83
nlxdodge <nlxdodge@gmail.com>
2022-12-03 07:22:14 +0100

    Add day 4 and update template

---
+++ 2022 - Kotlin/src/main/kotlin/AOC04.kt
@@ -0,0 +1,28 @@
+import utils.FileUtil
+
+class AOC04 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lines = FileUtil.getFileList("aoc01.txt")
+
+            val part1 = part1(lines)
+            val part2 = part2(lines)
+
+            println("Day 1 result 1: $part1")
+            println("Day 1 result 2: $part2")
+        }
+
+        private fun part1(input: List<String>): String {
+            for (line in input) {
+                println("Line: $line")
+            }
+            return input.first()
+        }
+
+        private fun part2(input: List<String>): String {
+            return input.first()
+        }
+    }
+}
+
