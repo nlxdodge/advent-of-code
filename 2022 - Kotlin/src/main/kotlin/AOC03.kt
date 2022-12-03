fcb6c9bcbc9f8c59a162476dc323d5badaaac968
nlxdodge <nlxdodge@gmail.com>
2022-12-03 06:54:02 +0100

    Added day 3 cleaned

---
+++ 2022 - Kotlin/src/main/kotlin/AOC03.kt
@@ -0,0 +1,47 @@
+import utils.FileUtil
+import kotlin.math.roundToInt
+
+class AOC03 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val lines = FileUtil.getFileList("aoc03.txt")
+
+            val part1 = part1(lines)
+            val part2 = part2(lines)
+
+            println("Day 3 result 1: $part1")
+            println("Day 3 result 2: $part2")
+        }
+
+        fun part1(input: List<String>): String {
+            var totalScore = 0
+            for (line in input) {
+                val half = (line.length / 2).toDouble().roundToInt()
+                val first = line.substring(0, half)
+                val last = line.substring(half, line.length)
+                for (chr in first) {
+                    if (last.contains(chr)) {
+                        totalScore += (chr.lowercaseChar() - (if (chr.isUpperCase()) 70 else 96)).code
+                        break
+                    }
+                }
+            }
+            return totalScore.toString()
+        }
+
+        fun part2(input: List<String>): String {
+            var totalScore = 0
+            for (lines in input.chunked(3)) {
+                for (chr in lines.get(0)) {
+                    if (lines[1].contains(chr) && lines[2].contains(chr)) {
+                        totalScore += (chr.lowercaseChar() - (if (chr.isUpperCase()) 70 else 96)).code
+                        break
+                    }
+                }
+            }
+            return totalScore.toString()
+        }
+    }
+}
+
