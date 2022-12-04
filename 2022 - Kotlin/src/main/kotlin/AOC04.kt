66e237b9baf9a675017964ce5aa8b24691477dd4
nlxdodge <nlxdodge@gmail.com>
2022-12-04 07:22:48 +0100

    Add dirty day 4

--- 2022 - Kotlin/src/main/kotlin/AOC04.kt
+++ 2022 - Kotlin/src/main/kotlin/AOC04.kt
@@ -1,27 +1,68 @@
 import utils.FileUtil
+import java.lang.StringBuilder
 
 class AOC04 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val lines = FileUtil.getFileList("aoc01.txt")
+            val day  = this::class.java.declaringClass.simpleName.substring(3, 5)
+            val lines = FileUtil.getFileList("AOC$day.txt")
 
             val part1 = part1(lines)
             val part2 = part2(lines)
 
-            println("Day 1 result 1: $part1")
-            println("Day 1 result 2: $part2")
+            println("Day $day result 1: $part1")
+            println("Day $day result 2: $part2")
         }
 
         private fun part1(input: List<String>): String {
+            var counter = 0
             for (line in input) {
-                println("Line: $line")
+                val pairs = mutableListOf<String>()
+                val lines = line.split(",")
+                for (ranges in lines) {
+                    val subrange = ranges.split("-")
+                    val from = subrange[0].toInt()
+                    val to = subrange[1].toInt()
+                    val items = StringBuilder()
+                    for (item in from.rangeTo(to)) {
+                        items.append("-")
+                        items.append(item)
+                        items.append("-")
+                    }
+                    pairs.add(items.toString())
+                }
+
+                if (pairs.get(0).contains(pairs.get(1)) || pairs.get(1).contains(pairs.get(0))) {
+                    counter++
+                }
             }
-            return input.first()
+            return counter.toString()
         }
 
         private fun part2(input: List<String>): String {
-            return input.first()
+            var counter = 0
+            for (line in input) {
+                val pairs = mutableListOf<List<Int>>()
+                for (ranges in line.split(",")) {
+                    val subrange = ranges.split("-")
+                    val from = subrange[0].toInt()
+                    val to = subrange[1].toInt()
+                    val items = mutableListOf<Int>()
+                    for (item in from.rangeTo(to)) {
+                        items.add(item)
+                    }
+                    pairs.add(items)
+
+                }
+                for (chr in pairs.get(0)) {
+                    if (pairs[1].contains(chr)) {
+                        counter++
+                        break
+                    }
+                }
+            }
+            return counter.toString()
         }
     }
 }
