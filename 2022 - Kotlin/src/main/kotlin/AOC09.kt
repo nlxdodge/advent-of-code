007c7847478de32c3f2de9e8203bb103ecb7befe
nlxdodge@gmail.com <nlxdodge@gmail.com>
2022-12-09 07:49:40 +0100

    Day 9 part 1 WIP

---
+++ 2022 - Kotlin/src/main/kotlin/AOC09.kt
@@ -0,0 +1,117 @@
+import utils.FileUtil
+import kotlin.math.abs
+
+class AOC09 {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val day = FileUtil.getDay()
+            val lines = FileUtil.getFileList("AOC$day.txt")
+
+            val part1 = part1(lines)
+            val part2 = part2(lines)
+
+            println("Day $day result 1: $part1")
+            println("Day $day result 2: $part2")
+        }
+
+        private fun part1(input: List<String>): String {
+            val size = 100
+            val grid = mutableListOf<MutableList<String>>()
+            for (x in 1..size) {
+                val lists = mutableListOf<String>()
+                for (y in 1..size) {
+                    lists.add(".")
+                }
+                grid.add(lists)
+            }
+            var hPos = IntRange(grid.size - 1, 0)
+            var oldHPos = hPos
+            var tPos = IntRange(grid.size - 1, 0)
+
+            for (command in input) {
+                val times = command.slice(IntRange(2, command.length - 1))
+                if (command.startsWith("R")) {
+                    for (t in 1..times.toInt()) {
+                        oldHPos = hPos
+                        hPos = IntRange(hPos.first, hPos.last + 1)
+                        if (!isTouching(hPos, tPos)) {
+                            grid[tPos.first][tPos.last] = "#"
+                            tPos = oldHPos
+                        }
+                        print(grid, hPos, tPos)
+                    }
+                }
+                if (command.startsWith("L")) {
+                    for (t in 1..times.toInt()) {
+                        oldHPos = hPos
+                        hPos = IntRange(hPos.first, hPos.last - 1)
+                        if (!isTouching(hPos, tPos)) {
+                            grid[tPos.first][tPos.last] = "#"
+                            tPos = oldHPos
+                        }
+                        print(grid, hPos, tPos)
+                    }
+                }
+                if (command.startsWith("U")) {
+                    for (t in 1..times.toInt()) {
+                        oldHPos = hPos
+                        hPos = IntRange(hPos.first - 1, hPos.last)
+                        if (!isTouching(hPos, tPos)) {
+                            grid[tPos.first][tPos.last] = "#"
+                            tPos = oldHPos
+                        }
+                        print(grid, hPos, tPos)
+                    }
+                }
+                if (command.startsWith("D")) {
+                    for (t in 1..times.toInt()) {
+                        oldHPos = hPos
+                        hPos = IntRange(hPos.first + 1, hPos.last)
+                        if (!isTouching(hPos, tPos)) {
+                            grid[tPos.first][tPos.last] = "#"
+                            tPos = oldHPos
+                        }
+                        print(grid, hPos, tPos)
+                    }
+                }
+            }
+            print(grid, IntRange(-1, -1), IntRange(-1, -1))
+            grid[grid.size - 1][0] = "s"
+            return grid.sumOf { it.count { x -> x == "#" } }.toString()
+        }
+
+        private fun part2(input: List<String>): String {
+            return ""
+        }
+
+        private fun isTouching(hPos: IntRange, tPos: IntRange): Boolean {
+            if (hPos.first == tPos.first && hPos.last == tPos.last) {
+                return false
+            }
+            val diffX = hPos.first - tPos.first
+            val diffY = hPos.last - tPos.last
+
+            if (abs(diffX) >= 2 || abs(diffY) >= 2) {
+                return false
+            }
+            return true
+        }
+
+        private fun print(grid: MutableList<MutableList<String>>, hPos: IntRange, tPos: IntRange) {
+            for (x in 0 until grid.size) {
+                for (y in 0 until grid.size) {
+                    if (hPos.first == x && hPos.last == y) {
+                        print("H")
+                    } else if (tPos.first == x && tPos.last == y) {
+                        print("T")
+                    } else {
+                        print(grid[x][y])
+                    }
+                }
+                println()
+            }
+            println("----------")
+        }
+    }
+}
