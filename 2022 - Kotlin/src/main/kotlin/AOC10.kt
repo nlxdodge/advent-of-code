77dd9fd4d27a9cdab341a6527e2d53983c070963
nlxdodge@gmail.com <nlxdodge@gmail.com>
2022-12-10 10:06:06 +0100

    Day 10 clean

--- 2022 - Kotlin/src/main/kotlin/AOC10.kt
+++ 2022 - Kotlin/src/main/kotlin/AOC10.kt
@@ -7,20 +7,21 @@ class AOC10 {
             val day = FileUtil.getDay()
             val lines = FileUtil.getFileList("AOC$day.txt")
 
-            val part1 = part1(lines)
+            val part1And2 = part1And2(lines)
 
-            println("Day $day result 1: $part1")
-            println("Day $day result 2 is visible in the terminal")
+            println("Day $day result 1: ${part1And2.first}")
+            println("Day $day result 2:")
+            printCRT(part1And2.second)
         }
 
-        private fun part1(input: List<String>): String {
+        private fun part1And2(input: List<String>): Pair<String, MutableList<MutableList<String>>> {
             var crt = mutableListOf(
-                ".".repeat(40).toMutableList(),
-                ".".repeat(40).toMutableList(),
-                ".".repeat(40).toMutableList(),
-                ".".repeat(40).toMutableList(),
-                ".".repeat(40).toMutableList(),
-                ".".repeat(40).toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
+                ".".repeat(40).map { it.toString() }.toMutableList(),
             )
             var signalStrength = 0
             var cycles = 1
@@ -38,34 +39,24 @@ class AOC10 {
                     }
                 }
             }
-            printCRT(crt)
-            return signalStrength.toString()
-        }
-
-        private fun part2() {
-
+            return Pair(signalStrength.toString(), crt)
         }
 
         private fun drawCRT(
-            crt: MutableList<MutableList<Char>>,
+            crt: MutableList<MutableList<String>>,
             cycle: Int,
             xStart: Int
-        ): MutableList<MutableList<Char>> {
+        ): MutableList<MutableList<String>> {
             val cycled = cycle % (6 * 40)
             val pos = IntRange(cycled % 40, cycled / 40)
             if (xStart == pos.first || xStart == pos.first - 1 || xStart == pos.first - 2) {
-                crt[pos.last][pos.first] = '█'
+                crt[pos.last][pos.first] = "█"
             }
             return crt
         }
 
-        private fun printCRT(crt: MutableList<MutableList<Char>>) {
-            for ((x, line) in crt.withIndex()) {
-                for ((y, chr) in line.withIndex()) {
-                    print(chr)
-                }
-                println()
-            }
+        private fun printCRT(crt: MutableList<MutableList<String>>) {
+            crt.forEach { line -> println(line.reduce { x, y -> x + y }) }
         }
 
         private fun checkSignalStrength(x: Int, cycles: Int): Int {
