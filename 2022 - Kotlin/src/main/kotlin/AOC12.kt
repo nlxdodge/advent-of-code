f4acdc83654e90b222800adc53f7782812cc41d6
NLxDoDge <nlxdodge@gmail.com>
2022-12-12 13:32:54 +0100

    Day 12 more groundwork

--- 2022 - Kotlin/src/main/kotlin/AOC12.kt
+++ 2022 - Kotlin/src/main/kotlin/AOC12.kt
@@ -1,5 +1,4 @@
 import utils.FileUtil
-import utils.GridUtil
 
 class AOC12 {
     companion object {
@@ -8,17 +7,20 @@ class AOC12 {
             val day = FileUtil.getDay()
             val lines = FileUtil.getFileList("AOC$day.txt")
 
-            var grid = GridUtil.generateMutableGridWithInput(lines)
 
-            val part1 = part1(lines)
+            var grid = generateMutableGridWithInput(lines)
+            val s = findCharPosition(grid, 'S'.code)
+            val e = findCharPosition(grid, 'E'.code)
+
+            val part1 = part1(grid, s, e)
             val part2 = part2(lines)
 
             println("Day $day result 1: $part1")
             println("Day $day result 2: $part2")
         }
 
-        private fun part1(grid: MutableList<MutableList<String>>): String {
-
+        private fun part1(grid: MutableList<MutableList<Node>>, start: Node, end: Node): String {
+            AStarPathFind(grid, start, end)
 
             return ""
         }
@@ -27,25 +29,76 @@ class AOC12 {
             return ""
         }
 
-        private fun AStarPathFind(grid: MutableList<MutableList<String>>, start: Node, end: Node) {
+        private fun AStarPathFind(grid: MutableList<MutableList<Node>>, start: Node, end: Node) {
             val cameFrom = mutableMapOf<Node, Node>()
             val openSet = mutableListOf(start)
 
             while (!openSet.isEmpty()) {
-                var current = openSet.minBy { Node::compareTo }
+                var current = openSet.minBy { x -> x.height }
+                if (current == end) {
+                    // found the path
+                    println("FOUND IT")
+                }
+                openSet.remove(current)
+                for (neighbor in findNeighbors(grid, current)) {
+                    val gScore = current.g + neighbor.height
+                    if (gScore < neighbor.g) {
+                        cameFrom[neighbor] = current
+                        neighbor.g = gScore
+                        neighbor.f = gScore + neighbor.height
+                        if (!openSet.contains(neighbor)) {
+                            openSet.add(neighbor)
+                        }
+                    }
+                }
             }
 
+        }
+
+        private fun findNeighbors(grid: MutableList<MutableList<Node>>, current: Node): MutableList<Node> {
+            val neighbours = mutableListOf<Node>()
+            if (current.x + 1 < grid.size) {
+                neighbours.add(grid[current.x + 1][current.y])
+            }
+            if (current.x - 1 > 0) {
+                neighbours.add(grid[current.x - 1][current.y])
+            }
+            if (current.y + 1 < grid[0].size) {
+                neighbours.add(grid[current.x][current.y + 1])
+            }
+            if (current.y - 1 > 0) {
+                neighbours.add(grid[current.x][current.y - 1])
+            }
+            return neighbours
+        }
+
+        private fun findCharPosition(grid: MutableList<MutableList<Node>>, find: Int): Node {
+            for (line in grid) {
+                for (chr in line) {
+                    if (chr.height == find) {
+                        return chr
+                    }
+                }
+            }
+            throw RuntimeException("Search for $find in grid failed!")
+        }
+
+        private fun generateMutableGridWithInput(input: List<String>): MutableList<MutableList<Node>> {
+            val grid = mutableListOf<MutableList<Node>>()
+            for ((x, line) in input.withIndex()) {
+                val currentLine = mutableListOf<Node>()
+                for ((y, chr) in line.withIndex()) {
+                    currentLine.add(Node(x, y, chr.code))
+                }
+                grid.add(currentLine)
+            }
+            return grid
         }
     }
 }
 
-class Node(val x: Int, val y: Int, val height: Int) : Comparable<Node> {
-    override fun compareTo(other: Node): Int {
-        return height.compareTo(other.height)
-    }
-
+class Node(val x: Int, val y: Int, var height: Int, var g: Int = Int.MAX_VALUE, var f: Int = Int.MAX_VALUE) {
     override fun toString(): String {
         return "Node(x: $x, y: $y, height: $height)"
     }
 }
-
