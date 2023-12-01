7f14c41a9cd0af96b4cadc104c35a0ef75ad2c54
NLxDoDge <nlxdodge@gmail.com>
2023-12-01 11:03:35 +0000

    2023 - day 1

--- 2023 - Rust/src/aoc01/aoc01.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut star1 = 0;
-    let mut star2 = 0;
-
-    println!("Day {} ⭐1️⃣  result: {}", 1, star1);
-    println!("Day {} ⭐2️⃣  result: {}", 1, star2);
-}
--- 2023 - Rust/src/main.rs
+++ 2023 - Rust/src/main.rs
@@ -1,19 +1,111 @@
 mod utils;
 
 fn main() {
-    let mut star1: i32 = 0;
-    let mut star2: i32 = 0;
-
     let filepath: &str = "src/inputs/aoc01.txt";
 
     let contents: &str = &utils::read_file(filepath.to_string());
 
-    let lines = contents.split("\r\n");
+    let lines = contents.split("\n");
 
-    for line in lines {
-        println!("{line}");
+    let star1: i32 = lines
+        .clone()
+        .map(|f| {
+            (find_first_number(f.to_string(), false).to_string()
+                + &find_last_number(f.to_string(), false).to_string())
+                .parse::<i32>()
+                .unwrap()
+        })
+        .sum();
+    let star2: i32 = lines
+        .clone()
+        .map(|f| {
+            (find_first_number(f.to_string(), true).to_string()
+                + &find_last_number(f.to_string(), true).to_string())
+                .parse::<i32>()
+                .unwrap()
+        })
+        .sum();
+
+    println!("Day 1 ⭐1️⃣  result: {}", star1);
+    println!("Day 1 ⭐2️⃣  result: {}", star2);
+}
+
+fn find_first_number(line: String, text_search: bool) -> i32 {
+    let mut string_number = String::new();
+    for chr in line.chars() {
+        if chr.is_alphabetic() && text_search {
+            string_number = string_number + &chr.to_string();
+            if string_number.contains("one") {
+                return 1;
+            }
+            if string_number.contains("two") {
+                return 2;
+            }
+            if string_number.contains("three") {
+                return 3;
+            }
+            if string_number.contains("four") {
+                return 4;
+            }
+            if string_number.contains("five") {
+                return 5;
+            }
+            if string_number.contains("six") {
+                return 6;
+            }
+            if string_number.contains("seven") {
+                return 7;
+            }
+            if string_number.contains("eight") {
+                return 8;
+            }
+            if string_number.contains("nine") {
+                return 9;
+            }
+        }
+        if chr.is_numeric() {
+            return chr.to_string().parse::<i32>().unwrap();
+        }
     }
+    return 0;
+}
 
-    println!("Day {} ⭐1️⃣  result: {}", utils::day(), star1);
-    println!("Day {} ⭐2️⃣  result: {}", utils::day(), star2);
+fn find_last_number(line: String, text_search: bool) -> i32 {
+    let mut string_number = String::new();
+    for chr in line.chars().rev() {
+        if chr.is_alphabetic() && text_search {
+            string_number = string_number + &chr.to_string();
+            if string_number.contains("eno") {
+                return 1;
+            }
+            if string_number.contains("owt") {
+                return 2;
+            }
+            if string_number.contains("eerht") {
+                return 3;
+            }
+            if string_number.contains("ruof") {
+                return 4;
+            }
+            if string_number.contains("evif") {
+                return 5;
+            }
+            if string_number.contains("xis") {
+                return 6;
+            }
+            if string_number.contains("neves") {
+                return 7;
+            }
+            if string_number.contains("thgie") {
+                return 8;
+            }
+            if string_number.contains("enin") {
+                return 9;
+            }
+        }
+        if chr.is_numeric() {
+            return chr.to_string().parse::<i32>().unwrap();
+        }
+    }
+    return 0;
 }
