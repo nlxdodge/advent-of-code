a2b177dcbd5175673855dd34caf06b235459d68a
NLxDoDge <nlxdodge@gmail.com>
2023-12-06 20:26:44 +0000

    2023 - day 6

--- 2023 - Rust/src/utils/mod.rs
+++ 2023 - Rust/src/utils/mod.rs
@@ -8,11 +8,23 @@ pub fn read_file(filepath: String) -> String {
 }
 
 pub fn string_to_vec_i64(string: &str) -> Vec<i64> {
-    return string
+    string
         .trim()
         .split(' ')
         .collect::<Vec<_>>()
         .iter()
+        .filter(|l| !l.is_empty())
         .map(|n| n.parse::<i64>().unwrap())
-        .collect();
+        .collect()
+}
+
+pub fn string_to_single_i64(string: &str) -> i64 {
+    string
+        .trim()
+        .split(' ')
+        .filter(|l| !l.is_empty())
+        .collect::<Vec<_>>()
+        .join("")
+        .parse::<i64>()
+        .unwrap()
 }
