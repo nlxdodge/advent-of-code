24528eb89d56affa511c096413a3197c3cfc6a26
NLxDoDge <nlxdodge@gmail.com>
2023-12-14 06:22:46 +0000

    2023 - day 10 wip

--- 2023 - Rust/src/main.rs
+++ 2023 - Rust/src/main.rs
@@ -1,70 +1,92 @@
-use std::collections::VecDeque;
-
 mod utils;
 
 fn main() {
-    let day = 9;
+    let day = 10;
     let filepath = format!("src/inputs/aoc{:0>2}.txt", day);
     let contents = &utils::read_file(filepath.to_string());
-    let lines: VecDeque<&str> = contents.lines().collect();
+    let lines: Vec<&str> = contents.lines().collect();
 
-    let sensory_data: Vec<SensoryData> = lines
-        .iter()
-        .map(|d| SensoryData {
-            points: d.split(' ').map(|n| n.parse::<i32>().unwrap()).collect(),
+    let max_size = 9;
+    let mut start_pos: Vec2D = Vec2D { x: 0, y: 0 };
+    let mut grid: Vec<Vec<char>> = vec![vec!['.'; max_size]; max_size];
+
+    for (index_y, y) in lines.iter().enumerate() {
+        for (index_x, x) in y.chars().enumerate() {
+            grid[index_x][index_y] = x;
+            if x == 'S' {
+                start_pos.x = index_x;
+                start_pos.y = index_y;
+            }
+        }
+    }
+    let mut current_pos = start_pos.clone();
+    check_for_valid_neighbor(&grid, &current_pos);
+
+    println!("Day {day} ⭐1️⃣  result: {:?}", start_pos);
+    // println!("Day {day} ⭐2️⃣  result: {star2}");
+}
+
+fn check_for_valid_neighbor(grid: &Vec<Vec<char>>, current_pos: &Vec2D) -> Vec<Vec2D> {
+    let mut neighbours: Vec<Vec2D> = vec![];
+    let check_below = Vec2D {
+        x: current_pos.x + 1,
+        y: current_pos.y,
+    };
+    if check_for_char(grid, &check_below, '|') {
+        neighbours.push(check_below);
+    }
+    let check_right = Vec2D {
+        x: current_pos.x + 1,
+        y: current_pos.y,
+    };
+    if check_for_char(grid, &check_right, '-') {
+        neighbours.push(check_right);
+    }
+
+    let check_right = Vec2D {
+        x: current_pos.x - 1,
+        y: current_pos.y,
+    };
+    if check_for_char(grid, &check_right, '-') {
+        neighbours.push(check_right);
+    }
+    if grid[current_pos.x][current_pos.y + 1] == '-' {
+        neighbours.push(Vec2D {
+            x: current_pos.x,
+            y: current_pos.y + 1,
         })
-        .collect();
-
-    let calculated_values: Vec<Vec<i32>> = sensory_data.iter().map(|sd| sd.calculate()).collect();
-    let star1: i32 = calculated_values
-        .iter()
-        .map(|data| data.get(1).unwrap())
-        .sum();
-    let star2: i32 = calculated_values
-        .iter()
-        .map(|data| data.first().unwrap())
-        .sum();
-
-    println!("Day {day} ⭐1️⃣  result: {star1}");
-    println!("Day {day} ⭐2️⃣  result: {star2}");
-}
-
-struct SensoryData {
-    points: Vec<i32>,
-}
-
-impl SensoryData {
-    fn calculate(&self) -> Vec<i32> {
-        let mut last_history: Vec<i32> = vec![];
-        let mut first_history: Vec<i32> = vec![];
-        let mut input = self.points.clone();
-        first_history.push(*input.first().unwrap());
-        last_history.push(*input.last().unwrap());
-        while get_diff(&input).iter().any(|n| n != &0) {
-            input = get_diff(&input);
-            first_history.push(*input.first().unwrap());
-            last_history.push(*input.last().unwrap());
-        }
-        input = get_diff(&input);
-        first_history.push(*input.first().unwrap());
-        last_history.push(*input.last().unwrap());
-        let mut last_first = 0;
-        let mut first_calculated: Vec<i32> = vec![];
-        for first_value in first_history.clone().iter().rev() {
-            last_first = first_value - last_first;
-            first_calculated.push(last_first);
-        }
-        vec![
-            *first_calculated.iter().rev().nth(0).unwrap(),
-            last_history.iter().sum(),
-        ]
     }
+
+    let check_below = Vec2D {
+        x: current_pos.x,
+        y: current_pos.y + 1,
+    };
+    if check_for_char(grid, &check_below, 'F') {
+        neighbours.push(check_below);
+    }
+
+    println!(
+        "Something: {} @ {} {}",
+        grid[current_pos.x][current_pos.y + 1],
+        current_pos.x,
+        current_pos.y + 1
+    );
+    neighbours
 }
 
-fn get_diff(input: &[i32]) -> Vec<i32> {
-    let mut new_input = vec![];
-    for window in input.windows(2) {
-        new_input.push(window[1] - window[0]);
+fn check_for_char(grid: &[Vec<char>], pos: &Vec2D, chr: char) -> bool {
+    if let Some(row) = grid.get(pos.x - 1) {
+        if let Some(column) = row.get(pos.y) {
+            if column == &chr {
+                return true;
+            }
+        }
     }
-    new_input
+    false
+}
+
+#[derive(Clone, Debug)]
+struct Vec2D {
+    x: usize,
+    y: usize,
 }
