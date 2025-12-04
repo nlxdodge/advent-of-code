ebebc023224fe5c838da67b6d79e6d463deb4306
NLxDoDge <nlxdodge@gmail.com>
2025-12-04 13:16:05 +0100

    2025: Day 3 (part 1 & 2)

--- 2025 - scala/src/main/scala/day/AOC3.scala
+++ 2025 - scala/src/main/scala/day/AOC3.scala
@@ -2,10 +2,43 @@ package day
 
 class AOC3 extends Day {
   def part1(): String = {
-    "Part 2 not implemented"
+    var battiesCounter = 0
+    val input = scala.io.Source.fromResource("aoc.txt").getLines().toList
+    for(line <- input) {
+      var batteries = ""
+      val lineListFull = line.split("").map(x => x.toInt).toList
+      val lineList = lineListFull.init
+      batteries += lineList.max.toString
+      val (before, after) = lineListFull.splitAt(lineListFull.indexOf(lineList.max) + 1)
+      batteries += after.max.toString
+      battiesCounter += batteries.toInt
+    }
+    battiesCounter.toString
   }
 
+
+
   def part2(): String = {
-    "Part 2 not implemented"
+    var battiesCounter = 0.toLong
+    val input = scala.io.Source.fromResource("aoc.txt").getLines().toList
+    for(line <- input) {
+      var batteries = ""
+      var lineList = line.split("").map(x => x.toInt).toList
+      for(end <- 12 to 1 by -1) {
+        val nextMaxInList = nextMax(lineList, lineList.length - end + 1)
+        batteries += nextMaxInList
+        lineList = lineList.splitAt(lineList.indexOf(nextMaxInList) + 1)._2
+      }
+      battiesCounter += batteries.toLong
+    }
+    battiesCounter.toString
+  }
+
+  def nextMax(list: List[Int], space: Int): Int = {
+    val (left, right) = list.splitAt(space)
+    if(left.isEmpty) {
+      return right.max
+    }
+    left.max
   }
 }
