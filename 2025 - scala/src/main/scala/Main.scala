8d5a248ce12f2290807f1e18957aed8aae2b0e5c
NLxDoDge <nlxdodge@gmail.com>
2025-12-04 13:16:34 +0100

    feat: add missing Scala files

---
+++ 2025 - scala/src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+@main def dayRunner(): Unit = {
+  val dayNum = 3
+  try {
+    val dayClass = Class.forName(s"day.AOC$dayNum")
+    val instance = dayClass.getDeclaredConstructor().newInstance().asInstanceOf[day.Day]
+    println(s"Day $dayNum")
+    println(s" ╠ Part 1: " + instance.part1())
+    println(s" ╚ Part 2: " + instance.part2())
+  } catch {
+    case e: ClassNotFoundException => println(s"Class for Day $dayNum not found.")
+  }
+}
+
